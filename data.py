cce8ea6db26219b73496d2ea79bbe101dd5f2113
Lian <lliao@email.sc.edu>
2024-04-25 03:52:16 -0400

    0425

--- data.py
+++ data.py
@@ -1,19 +1,36 @@
 import airsim
-import cv2
-import numpy as np
+import time
 
-# Connect to the AirSim simulator 
+# connect to the AirSim simulator
 client = airsim.CarClient()
 client.confirmConnection()
+client.enableApiControl(True)
+car_controls = airsim.CarControls()
 
-# Get camera images from the simulator
-responses = client.simGetImages([
-    airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)
-])
+while True:
+    # get state of the car
+    car_state = client.getCarState()
+    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
 
-for response in responses:
-    img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
-    img_rgb = img1d.reshape(response.height, response.width, 3)
+    # set the controls for car
+    car_controls.throttle = 1
+    car_controls.steering = 1
+    client.setCarControls(car_controls)
 
-    # Save or process the image
-    cv2.imwrite('frame.png', img_rgb)
+    # let car drive a bit
+    time.sleep(1)
+
+    # get camera images from the car
+    responses = client.simGetImages([
+        airsim.ImageRequest(0, airsim.ImageType.DepthVis),
+        airsim.ImageRequest(1, airsim.ImageType.DepthPlanar, True)])
+    print('Retrieved images: %d', len(responses))
+
+    # do something with images
+    for response in responses:
+        if response.pixels_as_float:
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+            airsim.write_pfm('py1.pfm', airsim.get_pfm_array(response))
+        else:
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+            airsim.write_file('py1.png', response.image_data_uint8)
